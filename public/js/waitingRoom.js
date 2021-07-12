0d32800307d8aeafb655d881b6a95188b22fec7b
thlien <60246645+thlien@users.noreply.github.com>
2021-07-12 16:43:14 -0700

    Cleaned Up WaitingRoom.js
    Commented WaitingRoom.js
    
    Moved WaitingRoom.js script tag to ensure the script only runs after all HTML nodes are loaded
    
    Deleted commented console logs
    
    Removed unnecessary code
    
    Renamed variables for better readability
    
    Increased modularization by moving code into reusable functions
    
    Refactored gameCode display node to change innerText instead of appending a new text node
    
    Refactored how the player list updates.  The logic is faster and the player list display is consistent across clients.
    
    Removed global variables by removing all variable declarations inside of the window event listener
    
    Moved the startWrap event listener outside of window event listerner.
    
    NOTE: For some reason, the startWrap event listener seems to be affected by all clients after a user clicks "start wrapping" for the first time. This is an issue we need to look into. It may however be fixed when we implement host game start only.

--- public/js/waitingRoom.js
+++ public/js/waitingRoom.js
@@ -1,63 +1,66 @@
-function addPlayerToList(pn){
-  var list = document.getElementById("playerList");
-  var items = list.getElementsByTagName("li");
-  console.log(pn);
-  for( var i = 0;i < items.length; ++i ){
-    if(items[i].innerText == pn){
-      return;
+function updateDisplay() {
+  var gameCode = getGameCode();
+
+  // Get roomCode element and display roomCode
+  let roomCode = document.getElementById("roomCode");
+  roomCode.innerText = "Room Code: " + gameCode;
+
+  // Retrieve a snapshot of all existing players in the current game
+  db.ref("game/"+gameCode).child("players").on("value", (snapshot) => {
+    // Get playerList element
+    var list = document.getElementById("playerList");
+
+    // Clear playerList
+    while (list.firstChild) {
+      list.removeChild(list.lastChild);
     }
-  }
+
+    // Update playerList
+    // For every player participating in the current game...
+    snapshot.forEach((player) => {
+      // Add player to playerList
+      addPlayerToList(player.key);
+    });
+  });
+};
+
+function getGameCode() {
+  // Extract query string from URL
+  let queryString = location.search.substring(1).split("&");
+
+  // Get gameCode from query string
+  let gameCodeQuery = queryString[1].split("=");
+
+  // Return gameCode
+  return gameCodeQuery[1];
+}
+
+function addPlayerToList(playerName){
+  // Get playerList element
+  var list = document.getElementById("playerList");
+
+  // Append playerName to playerList
   var item = document.createElement("li");
-  item.appendChild(document.createTextNode(pn));
+  item.appendChild(document.createTextNode(playerName));
   list.appendChild(item);
 };
 
-
 window.addEventListener("load", (event) => {
-  var sp = location.search.substring(1).split("&");
-  var temp = sp[0].split("=");
-  //console.log(temp);
-  var temp2 = sp[1].split("=");
-  //console.log(temp2);
-
-  if(temp[0] == "playerName"){
-    var code = temp2[1];
-    var hostName = temp[1];
-  }
-  else {
-    var code = temp[1];
-    var hostName = temp2[1];
-  }
-
-  var rc = document.getElementById("roomCode");
-  var rctxt = "Room Code: " + code;
-  var rctn = document.createTextNode(rctxt)
-  rc.appendChild(rctn); 
-
-
-  var ref = db.ref("game/"+code).child("players");
-  console.log(ref.toString());
-  ref.on("value", (snapshot) => {
-    //console.log("inside on");
-    //var players = snapshot.val();
-    //console.log(players);
-    snapshot.forEach((childSnapshot) => {
-      var key = childSnapshot.key;
-      //var value = childSnapshot.val();
-      //console.log(key);
-      //console.log(value.name);
-      addPlayerToList(key);
-    });
-  });
-  
-  let rs = document.getElementById("startWrap");
-  rs.addEventListener("click", (event) => {
-    var ref = db.ref("game/"+code).child("players");
-    ref.on("value", (snapshot) => {
-      if(snapshot.numChildren() > 1){
-        window.location.href = "./PrepareGift1.html"+location.search.substring();  
-      }
-    });
-  });
+  updateDisplay();
 });
 
+// Get startWrap element
+var startWrap = document.getElementById("startWrap");
+
+// Handle startWrap element click event
+startWrap.addEventListener("click", (event) => {
+  var gameCode = getGameCode();
+
+  // Retrieve a snapshot of all existing players in the current game
+  db.ref("game/"+gameCode).child("players").on("value", (snapshot) => {
+    // If the game has more more than one participant, allow the game to proceed
+    if(snapshot.numChildren() > 1) {
+      window.location.href = "./PrepareGift1.html"+location.search.substring();
+    }
+  });
+});
