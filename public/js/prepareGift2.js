4744ccac176d64caf6247bd768814d7118de176d
thlien <60246645+thlien@users.noreply.github.com>
2021-08-18 15:32:18 -0700

    Refactor prepareGift2.html and .js
    Refactored prepareGift2 to write gift color information to the database upon submission. Before, the color was written to the database the moment a user clicked on a color.

--- public/js/prepareGift2.js
+++ public/js/prepareGift2.js
@@ -1,50 +1,36 @@
-/**
- * Write gift box color information to Firebase
- * @param {string} color - gift box color
- */
-function chooseBoxColor(color) {
-	var gameCode = getGameCode();
-
-	getPlayerName((playerName) => {
-		getPlayerGiftCode(gameCode, playerName, (giftCode) => {
-			// Update user's box color
-			db.ref("game/" + gameCode).child("gift/" + giftCode).update({
-				boxColor: color
-		  });
-		})
-	});
-};
-
-/**
- * Write gift ribbon color information to Firebase
- * @param {string} color - gift ribbon color
- */
-function chooseRibbonColor(color) {
-	var gameCode = getGameCode();
-
-	getPlayerName((playerName) => {
-		getPlayerGiftCode(gameCode, playerName, (giftCode) => {
-			// Update user's box color
-			db.ref("game/" + gameCode).child("gift/" + giftCode).update({
-				ribbonColor: color
-		  });
-		})
-	});
-};
-
 // Handle click event for all elements named "box"
 document.getElementsByName("box").forEach((box) => {
   box.addEventListener('click', event => {
-    chooseBoxColor(box.value);
+    document.getElementById("boxColor").value = box.value;
   });
 });
 
 // Handle click events for all elements named "ribbon"
 document.getElementsByName("ribbon").forEach((ribbon) => {
   ribbon.addEventListener('click', event => {
-    chooseRibbonColor(ribbon.value);
+    document.getElementById("ribbonColor").value = ribbon.value;
   });
 });
+
+/**
+ * Write gift box and ribbon color information to Firebase
+ * @param {string} boxColor - gift box color
+ * @param {string} ribbonColor - gift ribbon color
+ * @param {string} callBack - asynchronous call back function
+ */
+function setGiftColor(boxColor, ribbonColor, callBack) {
+	var gameCode = getGameCode();
+
+	getPlayerName((playerName) => {
+		getPlayerGiftCode(gameCode, playerName, (giftCode) => {
+			// Update user's box and ribbon color
+			db.ref("game/" + gameCode).child("gift/" + giftCode).update({
+				ribbonColor: ribbonColor,
+				boxColor: boxColor
+		  }).then(callBack);
+		})
+	});
+}
 
 // Get roomSubmit element
 var roomSubmit = document.getElementById("roomSubmit");
@@ -55,14 +41,17 @@ roomSubmit.addEventListener("click", (event) => {
 
 	getPlayerName((playerName) => {
 		getPlayerGiftCode(gameCode, playerName, (giftCode) => {
-			// Update user's ribbon color
-			db.ref("game/" + gameCode).child("gift/" + giftCode).once("value", (snapshot) => {
-				if (snapshot.val().boxColor === undefined || snapshot.val().ribbonColor === undefined) {
-					window.alert("Please choose a box and ribbon color");
-				} else {
+			// Get box and ribbon color
+			var ribbonColor = document.getElementById("ribbonColor").value;
+			var boxColor = document.getElementById("boxColor").value;
+
+			if (boxColor === undefined || ribbonColor === undefined) {
+				window.alert("Please choose a box and ribbon color");
+			} else {
+				setGiftColor(boxColor, ribbonColor, () => {
 					window.location.href = "./PrepareGift3.html"+location.search.substring();
-				}
-			});
+				});
+			}
 		});
 	});
 });
