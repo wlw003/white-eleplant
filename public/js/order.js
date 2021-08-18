23b5ca64ee10a7870bbf95db9c2d647a7fd89329
thlien <60246645+thlien@users.noreply.github.com>
2021-08-18 14:07:57 -0700

    Fixed countdown timer bug in order.js
    The countdown timer function was not working properly. The countdown tick interval was not static as it was dependent on a dynamic variable. In addition, the countdown function was being called multiple times, depending on the number of players in the game, which also accelerated the countdown. These issues have been addressed and the count down timer is working properly now.
    
    I have also cleaned up the code and comments

--- public/js/order.js
+++ public/js/order.js
@@ -1,47 +1,46 @@
 /**
  * Function to go the next page. Players are given 3 seconds to read the order
- * page before they are automatically redirects to the next page. 
+ * page before they are automatically redirected to the next page. 
  */
 function goToNextPage(){
-  var timeleft = "";
   var countdown = document.getElementById("countdown");
 
-  // make the countdown timer starts at 3 seconds
-  countdown.appendChild(document.createTextNode(timeleft));
-  timeleft = 3;
-  countdown.innerText = timeleft;
+  // Count down for three seconds
+  var pageTimer = setInterval(() => {
+    // Decrement count
+    countdown.innerText = Number(countdown.innerText) - 1;
 
-  // countdown timer
-  var pageTimer = setInterval(function(){
-    countdown.innerText = timeleft;
-    timeleft--;
-    if(timeleft <= 0) {
-      clearInterval(pageTimer);
+    // Redirect to the next page when the countdown timer reaches 0 seconds
+    if (Number(countdown.innerText) < 1) {
       window.location.href = "./choosingGift.html"+location.search.substring();
     }
-  }, timeleft*1000);
-  
+  }, 1000);
 }
 
 window.addEventListener("load", (event) => {
   var gameCode = getGameCode();
 
-  // create game play history
-  getPlayerName((playerName) =>{
+  // Create game play history
+  getPlayerName((playerName) => {
     var newKey = db.ref().child("game/"+gameCode+"/history").push().key;
     var updates = {};
-    updates["game/"+gameCode+"/history"+"/"+newKey] = playerName + " is ready!";
-    db.ref().update(updates);
-  })
 
-  //make player order list
-  var ref = db.ref("game/"+gameCode).child("players");
+    updates["game/"+gameCode+"/history"+"/"+newKey] = playerName + " is ready!";
+
+    db.ref().update(updates);
+  });
+
   var list = document.getElementById("playerOrder");
-  ref.orderByChild("order").once("value", (snapshot) => {
+
+  db.ref("game/"+gameCode).child("players").orderByChild("order").once("value", (snapshot) => {
+    // Create player order list
     snapshot.forEach((childSnapshot) => {
       var name = childSnapshot.key;
+
       addPlayerToList(name, list);
-      goToNextPage();
     });
+
+    // Go to the next page after 3 seconds
+    goToNextPage();
   });
 });
