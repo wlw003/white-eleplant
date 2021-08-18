2ffa7e0a7ca275d6871f4f932f8ed9071900002f
thlien <60246645+thlien@users.noreply.github.com>
2021-08-18 16:09:53 -0700

    Cleaned up prepareGift3.js

--- public/js/prepareGift3.js
+++ public/js/prepareGift3.js
@@ -1,32 +1,27 @@
 /**
- * Function that checks if all players finished entering gift information and 
- * are ready to go on to the page
+ * Function that redirects players to the next page after all players submit their gift's color
  * @param {int} gameCode 
  */
  function everyoneReadyEvent(gameCode){
   db.ref("game/"+gameCode).once("value").then((snapshot) =>{
-
-    // get number of players
+    // Get number of players
     var playerNum = snapshot.child("players").numChildren();
 
-    //get number of ready gift
-    var list = document.getElementById("playerOrder");
-    var giftNum = list.children.length;
-    console.log("giftNum: " + giftNum);
+    // Get number of ready gifts
+    var giftNum = document.getElementById("playerOrder").children.length;
 
-    // go to the next page when the numbers of players and gift are equal
-    if(playerNum == giftNum){
+    // Go to the next page when the numbers of players and ready gifts are equal
+    if (playerNum == giftNum) {
       window.location.href = "./order.html" + location.search.substring();
     }
   });
 };
 
 window.addEventListener("load", (event) => {
-
   var gameCode = getGameCode();
-  db.ref("game/"+gameCode+"/gift").on("value", (snapshot) =>{
 
-    // get player list
+  db.ref("game/"+gameCode+"/gift").on("value", (snapshot) =>{
+    // Get player list
     var list = document.getElementById("playerOrder");
 
       // Clear playerList
@@ -36,17 +31,17 @@ window.addEventListener("load", (event) => {
   
       // Update playerList
       snapshot.forEach((gift) => {
-        // Add player to playerList
-        var box = gift.child("boxColor").exists();
-        var ribbon = gift.child("ribbonColor").exists();
-        if(box && ribbon){
-          var playerName = gift.child("name").val(); 
+        var boxColor = gift.child("boxColor").exists();
+        var ribbonColor = gift.child("ribbonColor").exists();
+
+        // Add ready players to playerList
+        if(boxColor && ribbonColor){
+          var playerName = gift.child("name").val();
+
           addPlayerToList(playerName, list);
         }
       });
+
       everyoneReadyEvent(gameCode);
   });
-
 });
-
-
