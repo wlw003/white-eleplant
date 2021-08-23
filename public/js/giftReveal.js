97c84520a6e6f17fa8132f8748ca9f29b1f5ccb2
wlw003 <winglamwong316@gmail.com>
2021-08-23 14:59:44 -0700

    Add comments and refactor code in giftReveal.js

--- public/js/giftReveal.js
+++ public/js/giftReveal.js
@@ -1,46 +1,51 @@
+/**
+ * Function to create a hyperlink with the gift information. The gift 
+ * description will be the clickable text that leds to the gift URL.
+ * @param {string} gameCode current game code
+ * @param {string} giftID current gift ID
+ */
+function createLink(gameCode, giftID){
+  db.ref("game/"+gameCode+"/gift/"+giftID).once("value").then((snapshot) => {
+    // get gift description
+    var giftDes = snapshot.child("description").val();
+    // get gift URL
+    var link = snapshot.child("link").val();
+    var item = document.getElementById("giftDes");
+    // create a hyperlink with given info
+    var a = document.createElement("a");
+    a.appendChild(document.createTextNode(giftDes));
+    a.title = giftDes;
+    a.href = link;
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
+    item.appendChild(a);
+  });
+}
+
 window.addEventListener("load", (event) => {
-  var sp = location.search.substring(1).split("&");
-  var temp = sp[0].split("=");
-  //console.log(temp);
-  var temp2 = sp[1].split("=");
-  //console.log(temp2);
+  var gameCode = getGameCode();
 
-  if(temp[0] == "playerName"){
-    var code = temp2[1];
-    var hostName = temp[1];
-  }
-  else {
-    var code = temp[1];
-    var hostName = temp2[1];
-  }
-
-  var ref = db.ref("game/"+code);
-  console.log(ref.toString());
+  var ref = db.ref("game/"+gameCode);
+  
+  // get the most recently played player's gift info
   ref.child("history").limitToLast(1).once("value").then((snapshot) => {
     //console.log("inside on");
     snapshot.forEach((childSnapshot) => {
+
+      // get player's name for the banner
       var currPlayer = childSnapshot.child("currPlayer").val();
-      console.log("currPlayer: "+currPlayer);
+      //console.log("currPlayer: "+currPlayer);
       var name = document.getElementById("currName");
       name.appendChild(document.createTextNode(currPlayer));
+
+      // get gift description for the main content body
       var id = childSnapshot.child("gift").val();
-      console.log("id "+ id);
-      ref.child("gift/"+id).once("value").then((snapshot2) => {
-        var giftDes = snapshot2.child("description").val();
-        //console.log(giftDes);
-        var link = snapshot2.child("link").val();
-        var item = document.getElementById("giftDes");
-        var a = document.createElement("a");
-        a.appendChild(document.createTextNode(giftDes))
-        a.title = giftDes;
-        a.href = link;
-        a.target = "_blank";
-        a.rel = "noopener noreferrer";
-        item.appendChild(a);
-      });
+      //console.log("id "+ id);
+      createLink(gameCode, id);
     });
   });
   
+  // button to go to the next page
   let rs = document.getElementById("gotIt");
   rs.addEventListener("click", (event) => {
     window.location.href = "./choosingGift.html"+location.search.substring();  
