d8d609c48ffd316be256724e50c16be5083b703b
thlien <60246645+thlien@users.noreply.github.com>
2021-08-25 12:35:21 -0700

    Cleaned Up Code in revealGift.js
    Deleted console logs
    
    Deleted unnecessary variables
    
    Rewrote some comments for better clarity
    
    Added vertical spacing for better readability
    
    Moved "Got it" click event handler outside of load event handler

--- public/js/giftReveal.js
+++ public/js/giftReveal.js
@@ -1,54 +1,48 @@
 /**
- * Function to create a hyperlink with the gift information. The gift 
- * description will be the clickable text that leds to the gift URL.
+ * Function that creates a hyperlink using gift information. The gift 
+ * description will be used as clickable text, leading to the gift URL.
  * @param {string} gameCode current game code
  * @param {string} giftID current gift ID
  */
-function createLink(gameCode, giftID){
+function createLink(gameCode, giftID) {
   db.ref("game/"+gameCode+"/gift/"+giftID).once("value").then((snapshot) => {
-    // get gift description
-    var giftDes = snapshot.child("description").val();
-    // get gift URL
-    var link = snapshot.child("link").val();
-    var item = document.getElementById("giftDes");
-    // create a hyperlink with given info
-    var a = document.createElement("a");
-    a.appendChild(document.createTextNode(giftDes));
-    a.title = giftDes;
-    a.href = link;
-    a.target = "_blank";
+    // Create link tag element
+    var a = document.createElement('a');
+
+    // Define link tag attributes
+    a.textContent = snapshot.child("description").val();
+    a.title = snapshot.child("description").val();
+    a.href = snapshot.child("link").val();
     a.rel = "noopener noreferrer";
-    item.appendChild(a);
+    a.target = "_blank";
+
+    // Append link tag element to giftDes element in DOM
+    document.getElementById("giftDes").appendChild(a);
   });
 }
 
 window.addEventListener("load", (event) => {
   var gameCode = getGameCode();
-
-  var ref = db.ref("game/"+gameCode);
   
-  // get the most recently played player's gift info
-  ref.child("history").limitToLast(1).once("value").then((snapshot) => {
-    //console.log("inside on");
+  // Get the most current player's gift information
+  db.ref("game/"+gameCode).child("history").limitToLast(1).once("value").then((snapshot) => {
     snapshot.forEach((childSnapshot) => {
-
-      // get player's name for the banner
-      var currPlayer = childSnapshot.child("currPlayer").val();
-      //console.log("currPlayer: "+currPlayer);
+      // Get currName element
       var name = document.getElementById("currName");
-      name.appendChild(document.createTextNode(currPlayer));
 
-      // get gift description for the main content body
-      var id = childSnapshot.child("gift").val();
-      //console.log("id "+ id);
-      createLink(gameCode, id);
+      // Display current player's name as present chooser
+      name.textContent = childSnapshot.child("currPlayer").val();
+
+      // Display hyperlinked gift description in main content
+      createLink(gameCode, childSnapshot.child("gift").val());
     });
-  });
-  
-  // button to go to the next page
-  let rs = document.getElementById("gotIt");
-  rs.addEventListener("click", (event) => {
-    window.location.href = "./choosingGift.html"+location.search.substring();  
   });
 });
 
+// Get GotIt element
+let rs = document.getElementById("gotIt");
+
+// Handle gotIt element click event
+rs.addEventListener("click", (event) => {
+  window.location.href = "./choosingGift.html"+location.search.substring();
+});
